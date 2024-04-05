07da42b0b7fcbc3fa313e6e05a52dd5a71e06d81
Paige Rolling <95886374+prolling@users.noreply.github.com>
2024-04-04 21:11:21 -0500

    Delete DropTables.sql

--- DropTables.sql
+++
@@ -1,9 +0,0 @@
--- this script deletes all tables
-
-DROP TABLE minor;
-DROP TABLE major;
-DROP TABLE register;
-DROP TABLE courses;
-DROP TABLE degrees;
-DROP TABLE departments;
-DROP TABLE students;
